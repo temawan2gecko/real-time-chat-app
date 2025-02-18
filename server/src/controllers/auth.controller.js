9e8d00455b7f03a0994eb33f780babd55afcd006
tenawan2gecko <artem.tchalunin@yandex.ru>
2025-02-18 22:31:01 +0300

    Create a backend for messages

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 import User from "../models/user.model.js";
 import { generateToken } from "../lib/utils.js";
 
@@ -12,7 +13,7 @@ export const signup = async (req, res) => {
         .status(400)
         .json({ message: "Все поля обязательны для заполнения." });
     }
-    
+
     if (password.length < 6) {
       return res
         .status(400)
@@ -52,9 +53,68 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: "Непредвиденная ошибка сервера" });
   }
 };
-export const login = (req, res) => {
-  res.send("login route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Неверные учетные данные" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Неверные учетные данные" });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Внутренняя ошибка сервера" });
+  }
 };
 export const logout = (req, res) => {
-  res.send("logout route");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Выход совершен успешно" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ message: "Внутренняя ошибка сервера" });
+  }
+};
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Загрузите фотографию профиля" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("error in update profile:", error);
+    res.status(500).json({ message: "Внутренняя ошибка сервера" });
+  }
+};
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Внутренняя ошибка сервера" });
+  }
 };
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser'
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from './routes/message.route.js'
 
 dotenv.config();
 const app = express();
@@ -10,7 +12,9 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser())
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on port PORT:", PORT);
